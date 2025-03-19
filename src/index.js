464e644d0d19b7a1257af5de2553890e3fa375a2
Hugo Ghesquier <hugo.ghesquier59@gmail.com>
2025-03-19 12:41:07 +0100

    update package.json to set main entry point and add test script; define User, Account, Session, VerificationToken, and Authenticator models in schema.prisma; modify index.js for improved API response structure

--- src/index.js
+++ src/index.js
@@ -1,31 +1,40 @@
-import express from "express";
-import dotenv from "dotenv";
-import cors from "cors";
-import { PrismaClient } from "@prisma/client";
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+//const { PrismaClient } = require('@prisma/client');
 
 dotenv.config();
 const app = express();
-const prisma = new PrismaClient();
+//const prisma = new PrismaClient();
 
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("API Babyfoot en ligne !");
+  res.json({ 
+    status: "online",
+    message: "Babyfoot Tracker API is up and running!",
+    version: "0.1.0",
+    documentation: "/api/docs" // Dispoible dans la prochaine version de l'API
+  });
 });
 
-// Exemple de route pour ajouter un utilisateur
+// Création d'un utilisateur
 app.post("/users", async (req, res) => {
   const { name, discordId } = req.body;
   try {
-    const user = await prisma.user.create({
+    /*const user = await prisma.user.create({
       data: { name, discordId },
-    });
-    res.json(user);
+    });*/
+    //res.json(user);
+    res.json({ name, discordId });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 });
+
+// Modification d'un utilisateur
+// Récupération des utilisateurs pour le leaderboard 
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Serveur lancé sur http://localhost:${PORT}`));
