1d1883c44a98c0e95e0182061dbff09cd7654173
Hugo Ghesquier <hugo.ghesquier59@gmail.com>
2025-03-19 14:35:15 +0100

    refactor user creation endpoint to use Player model; update request body to include username instead of name

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
-//const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require('@prisma/client');
 
 dotenv.config();
 const app = express();
-//const prisma = new PrismaClient();
+const prisma = new PrismaClient();
 
 app.use(cors());
 app.use(express.json());
@@ -21,18 +21,18 @@ app.get("/", (req, res) => {
 
 // Création d'un utilisateur
 app.post("/users", async (req, res) => {
-  const { name, discordId } = req.body;
+  const { username, discordId} = req.body;
   try {
-    /*const user = await prisma.user.create({
-      data: { name, discordId },
-    });*/
-    //res.json(user);
-    res.json({ name, discordId });
+    const user = await prisma.player.create({
+      data: { username, discordId },
+    });
+    res.json(user);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 });
 
+// Récupération des informations d'un utilisateur
 // Modification d'un utilisateur
 // Récupération des utilisateurs pour le leaderboard 
 
